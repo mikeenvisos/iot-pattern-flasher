10c56694d5ee3e05c2910ac0238aa47dbe3d174a
Mike Hill <mike@envisos.com>
2020-04-17 01:31:08 +0400

    Cleaned up redundant methods and removed local patten XML.

--- PatternFlasher/Program.cs
+++ PatternFlasher/Program.cs
@@ -163,79 +163,7 @@ namespace PatternFlasher
             }
         }
 
-        public static async Task ListenIoTHub()
-        {
-            
-            // Listens for events on IoT Hub
-            Console.WriteLine("IoT Hub Quickstarts - Read device to cloud messages. Ctrl-C to exit.\n");
-
-
-            // Create an EventHubClient instance to connect to the
-            // IoT Hub Event Hubs-compatible endpoint.
-            var connectionString = new EventHubsConnectionStringBuilder(new Uri(s_eventHubsCompatibleEndpoint), s_eventHubsCompatiblePath, s_iotHubSasKeyName, s_iotHubSasKey);
-            s_eventHubClient = EventHubClient.CreateFromConnectionString(connectionString.ToString());
-
-            // Create a PartitionReciever for each partition on the hub.
-            var runtimeInfo = await s_eventHubClient.GetRuntimeInformationAsync();
-            var d2cPartitions = runtimeInfo.PartitionIds;
-
-            CancellationTokenSource cts = new CancellationTokenSource();
-
-            Console.CancelKeyPress += (s, e) =>
-            {
-                e.Cancel = true;
-                cts.Cancel();
-                Console.WriteLine("Exiting...");
-            };
-
-            var tasks = new List<Task>();
-            foreach (string partition in d2cPartitions)
-            {
-                tasks.Add(ReceiveMessagesFromDeviceAsyncV2(partition, cts.Token));
-            }
-
-            // Wait for all the PartitionReceivers to finsih.
-            Task.WaitAll(tasks.ToArray());
-
-        }
-
         public static async Task ReceiveMessagesFromDeviceAsync(string partition, CancellationToken ct)
-        {
-            // Create the receiver using the default consumer group.
-            // For the purposes of this sample, read only messages sent since 
-            // the time the receiver is created. Typically, you don't want to skip any messages.
-            var eventHubReceiver = s_eventHubClient.CreateReceiver("$Default", partition, EventPosition.FromEnqueuedTime(DateTime.Now));
-            Console.WriteLine("Create receiver on partition: " + partition);
-            while (true)
-            {
-                if (ct.IsCancellationRequested) break;
-                Console.WriteLine("Listening for messages on: " + partition);
-                // Check for EventData - this methods times out if there is nothing to retrieve.
-                var events = await eventHubReceiver.ReceiveAsync(100);
-
-                // If there is data in the batch, process it.
-                if (events == null) continue;
-
-                foreach (EventData eventData in events)
-                {
-                    string data = Encoding.UTF8.GetString(eventData.Body.Array);
-                    Console.WriteLine("Message received on partition {0}:", partition);
-                    Console.WriteLine("  {0}:", data);
-                    Console.WriteLine("Application properties (set by device):");
-                    foreach (var prop in eventData.Properties)
-                    {
-                        Console.WriteLine("  {0}: {1}", prop.Key, prop.Value);
-                    }
-                    Console.WriteLine("System properties (set by IoT Hub):");
-                    foreach (var prop in eventData.SystemProperties)
-                    {
-                        Console.WriteLine("  {0}: {1}", prop.Key, prop.Value);
-                    }
-                }
-            }
-        }
-
-        public static async Task ReceiveMessagesFromDeviceAsyncV2(string partition, CancellationToken ct)
         {
             // Create the receiver using the default consumer group.
             // For the purposes of this sample, read only messages sent since 
@@ -334,7 +262,7 @@ namespace PatternFlasher
             var tasks = new List<Task>();
             foreach (string partition in d2cPartitions)
             {
-                tasks.Add(ReceiveMessagesFromDeviceAsyncV2(partition, cts.Token));
+                tasks.Add(ReceiveMessagesFromDeviceAsync(partition, cts.Token));
             }
 
             Task.WaitAll(tasks.ToArray());
